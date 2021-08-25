f3d5e60c4fb4e60ac16ed4ee195f63ad0927f038
Diksha Satpute <dikshasatpute@boomi.com>
2021-08-25 11:12:14 -0400

    Remove element non-eff

---
+++ Solutions/RemoveElement.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class RemoveElement {
+
+    
+    public static int removeElements(int array[]) {
+        int num = 2; int count = 0;
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        for(int i=0; i<array.length; i++) {
+
+            if(array[i] == num) {
+                count++;
+            }
+
+            if(array[i] > num) {
+                array[i-count] = array[i];
+            }
+        }
+
+        return  (array.length)-count;
+    }
+
+    public static void main(String[] args) {
+        int array[] = {0,1,2,2,3,0,4,2};
+        int finalResult = removeElements(array);
+        for(int i=0;i<finalResult;i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
